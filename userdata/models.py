e9ea8258aa5cb64e4c3006e8ab248a8fc8fb1c9d
Bry <etoquillab@gmail.com>
2026-04-04 20:46:39 +0800

    add database and model

--- userdata/models.py
+++ userdata/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class userdata(models.Model):
+    user_id = models.CharField(max_length=10, unique=True, editable=False, blank=True)
+    firstname = models.CharField(max_length=225)
+    lastname = models.CharField(max_length=225)
+    username = models.CharField(max_length=225, unique=True)
+    password = models.CharField(max_length=225)
+
+    def save(self, *args, **kwargs):
+        if not self.user_id:
+            last_user = userdata.objects.all().order_by('user_id').last()
+            if last_user and last_user.user_id and len(last_user.user_id) > 1:
+                try:
+                    last_id = int(last_user.user_id[1:])
+                    new_id = last_id + 1
+                except ValueError:
+                    new_id = 1
+            else:
+                new_id = 1
+            self.user_id = f'A{new_id:03d}'
+        super().save(*args, **kwargs)
--- userdata/views.py
+++ userdata/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+
+from .models import userdata as Userdata
+
 
 def login(request):
-    return render(request, 'authentication/login.html',{})
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        userchecker = authenticate(request, username=username, password=password)
+        if userchecker is not None:
+            auth_login(request, userchecker)
+            return redirect('home')
+        messages.error(request, 'Invalid username or password')
+    return render(request, 'home.html', {})
+
 
 def logout(request):
-    return redirect('login')
+    auth_logout(request)
+    return redirect('user_login')
+
 
 def register(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('first_name', '')
+        lastname = request.POST.get('last_name', '')
+        username = request.POST.get('username', '')
+        password = make_password(request.POST.get('password', ''))
+
+        Userdata.objects.create(
+            firstname=firstname,
+            lastname=lastname,
+            username=username,
+            password=password,
+        )
+        return redirect('user_login')
     return render(request, 'userprofile/register.html')
-
-
